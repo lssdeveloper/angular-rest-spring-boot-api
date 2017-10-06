e9f5fdf7c265107afdb22ba09e8efad8a85b9774
Leandro Serra <leandro.serra.10@gmail.com>
2017-10-06 19:44:33 -0300

    O que é o CORS?

--- money-api/src/main/java/com/lssdeveloper/money/api/resource/CategoriaResource.java
+++ money-api/src/main/java/com/lssdeveloper/money/api/resource/CategoriaResource.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -43,6 +44,7 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService categoriaService;
 	
+	@CrossOrigin(maxAge = 10, origins = {"http://localhost:8000"})
 	@GetMapping	
 	public List<Categoria> listar(){
 		//findAll() = Select * from categoria
